523f0edb04de46187d8843592e61b7236e3590fb
igrekus <upload234@mail.ru>
2021-04-14 17:56:21 +0300

    WIP: update main window logic

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,7 @@
 import os
 import serial
 from PyQt6 import uic
-from PyQt6.QtCore import QTimer, QTime
+from PyQt6.QtCore import QTimer, QTime, Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QMainWindow
 # from easymodbus.run import modbusClient
@@ -17,6 +17,12 @@ class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+
+        self.setAttribute(Qt.WidgetAttribute.WA_QuitOnClose)
+        self.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+
+        self._ui = uic.loadUi("mainwindow.ui", self)
+
         # - private void MainForm_Load(object sender, EventArgs e)
         self.recipe = Recipe()  # -   recipies
         self.settings = Settings()  # -   settings
@@ -72,8 +78,77 @@ class MainWindow(QMainWindow):
         self.process_end = False
         self.venting = False
         self.lid_closing = False
+        self.mfc_read = []
+        self.ar_mfc_read = []
+        self.o2_mfc_read = []
+        self.cf4_mfc_read = []
+        self.n2_mfc_read = []
+        self.sccm_ar = 0
+        self.sccm_o2 = 0
+        self.sccm_cf4 = 0
+        self.sccm_n2 = 0
+        self.mw_power = 0
+        self.generator_hb = []
 
-        # self.setAttribute(Qt.WA_QuitOnClose)
-        # self.setAttribute(Qt.WA_DeleteOnClose)
+        self.read_recipes_from_folder()
 
-        self._ui = uic.loadUi("mainwindow.ui", self)
+    def read_recipes_from_folder(self):  # reading recipes from the folder
+        path = "recipes"
+
+        dir_list = os.listdir(path)
+        rcp_list = []
+
+        if dir_list:    #  checking emptylness list
+            for file_name in os.listdir(path):  #   addiing rcp to list
+                if file_name.endswith('.rcp'):
+                    rcp_list.append(file_name)
+
+        for rcp in rcp_list:    #   adding list to comborecipies
+            self._ui.comboRecipes.addItem(rcp)
+
+    def on_serial_data_received(self):
+        # TODO getting data from com port
+        pass
+
+    #   timers' functions
+    def on_timed_process_event(self, source):
+        self.process_end = True
+        # todo remake
+
+    def on_timed_ignition_event(self, source, event):
+        self.ignition_start = False
+        # todo remake
+
+    def on_timed_send_received_modbus(self, source, event):
+        modbusClient.connect()
+        self.generator_hb = modbusClient.read_coils(self.settings.generator_hb,1)
+
+        if self.lid_up_button:
+            modbusClient.WriteSingleCoil(self.settings.lid_up_button, True)
+            self.lid_up_button = False
+
+        if self.lid_down_button:
+            modbusClient.WriteSingleCoil(self.settings.lid_up_button, False)
+            self.lid_down_button = False
+
+        if self.vent_button:
+            self.close_all_gas_valves()
+            self.pump_valve.open()
+            self.vent_valve.open()
+            self.gas_out_valve.open()
+          #  todo self.timer_pump_for_vent.start()
+            self.vent_button = False
+
+        if self.process_end:
+            self.process_end = False
+            self.proccess_started = False
+            self.process_time = 0
+            self.close_all_gas_valves()
+            modbusClient.write_single_register(self.settings.mw_onoff,0)
+            modbusClient.write_single_coil(self.settings.mw_apply_bit, True)
+            modbusClient.write_single_coil(self.settings.ignition, False)
+            self.valve_port.write('P90\r\n')
+            # todo update labelState text => 'process is ended'
+
+    def close_all_gas_valves(self):
+        pass
