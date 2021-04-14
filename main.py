10bd443eb8c67bf583513523265973ef39825032
igrekus <upload234@mail.ru>
2021-04-14 14:26:06 +0300

    MISC: add entry point

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+from mainwindow import MainWindow
+
+
+def main(args):
+    app = QApplication(args)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main(sys.argv)
