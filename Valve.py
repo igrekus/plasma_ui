374e7054bfb23ec9581055941f1b5899e0bb65cb
igrekus <upload234@mail.ru>
2021-10-25 17:46:59 +0300

    wip

--- Valve.py
+++ Valve.py
@@ -1,48 +1,47 @@
+from easymodbus.run import ModbusClient
+
+
 class Valve:
 
-    def __init__(self,
-                 valve_address,
-                 valve_opened,
-                 picture_image):
+    def __init__(self, valve_address, valve_opened, ):
         self.valve_address = valve_address
         self.valve_opened = valve_opened
-        self.picture_image = picture_image
 
     def close(self):
         self.valve_opened = False
+        self.modbusClient.write_single_coil(self.valve_address, self.valve_opened)
         ##TODO implement valve state close
 
     def open(self):
         self.valve_opened = True
+        modbusClient.write_single_coil(self.valve_address, self.valve_opened)
         ##TODO implement valve state open
 
     def is_opened(self):
-        ##TODO implement valve state is_open
-        pass
+        self.valve_opened = modbusClient.read_coils(self.valve_address, 0)
+        return self.valve_opened
 
 
 class GasValve(Valve):
 
-    def __init__(self,
-                 mfc_enable_address,
-                 mfc_flow_address,
-                 flow_rate,
-                 valve_address,
-                 valve_opened,
-                 picture_image):
-        super().__init__(valve_address,
-                         valve_opened,
-                         picture_image)
+    def __init__(self, valve_address, valve_opened, flow_rate, mfc_enable_address, mfc_flow_address, new_flow_rate=0.0):
+        super().__init__(valve_address, valve_opened)
+        self.flow_rate = new_flow_rate
         self.mfc_enable_address = mfc_enable_address
         self.mfc_flow_address = mfc_flow_address
-        self.mfc_flow_address = flow_rate
+        self.mfc_flow_rate = flow_rate
 
-    def start_flow(self, new_flow_rate):
+    def start_flow(self):
         self.valve_opened = True
-        self.flow_rate = new_flow_rate
-        ##TODO implement valve state start_flow
+        modbusClient.write_single_coil(self.valve_address, self.valve_opened)
+        modbusClient.write_single_coil(self.mfc_enable_address, self.valve_opened)
+        modbusClient.write_multiple_registers(self.mfc_flow_address,
+                                              modbusClient.convert_registers_to_float(self.flow_rate))
 
     def close(self):
         self.valve_opened = False
-        self.flow_rate = 0
-        ##TODO implement valve state start_flow_close
+        self.flow_rate = 0.0
+        modbusClient.write_single_coil(self.valve_address, self.valve_opened)
+        modbusClient.write_single_coil(self.mfc_enable_address, self.valve_opened)
+        modbusClient.write_multiple_registers(self.mfc_flow_address,
+                                              modbusClient.convert_registers_to_float(self.flow_rate))
--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 
 
-def main(args):
-    app = QApplication(args)
-    window = MainWindow()
-    window.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    mw.show()
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
